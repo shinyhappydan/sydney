daefab6eba0c089d2471bde8f0ebb2bdbb2556a3
Daniel Bell <daniel.bell@cern.ch>
2025-04-15 17:49:02 +0200

    only run e2e tests on webkit for now

--- playwright.config.ts
+++ playwright.config.ts
@@ -45,18 +45,18 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    {
-      name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-      },
-    },
-    {
-      name: 'firefox',
-      use: {
-        ...devices['Desktop Firefox'],
-      },
-    },
+    // {
+    //   name: 'chromium',
+    //   use: {
+    //     ...devices['Desktop Chrome'],
+    //   },
+    // },
+    // {
+    //   name: 'firefox',
+    //   use: {
+    //     ...devices['Desktop Firefox'],
+    //   },
+    // },
     {
       name: 'webkit',
       use: {
