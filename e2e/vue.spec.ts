4a3e872538684a56b9ea83644948946ba348826f
Daniel Bell <daniel.bell@cern.ch>
2025-04-15 14:31:43 +0200

    add end to end test for creating a recipe

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -1,6 +1,15 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test'
 
 test('visits the home page', async ({ page }) => {
-  await page.goto('/');
-  await expect(page.locator('h1')).toHaveText('Recipes');
+  await page.goto('/')
+  await expect(page.locator('h1')).toHaveText('Recipes')
+})
+
+test('creates a recipe', async ({ page }) => {
+  await page.goto('/')
+  await page.click('text=Create Recipe')
+  await page.fill('input', 'banana bread')
+  await page.click('text=Submit')
+  await expect(page.locator('h1')).toHaveText('Recipes')
+  await expect(page.getByText('banana bread')).toBeVisible()
 })
